9c416c7e69e1044c9ea942ed0a32b073ad51d7db
Christian Arevalo <carevaloq87@hotmail.com>
2020-02-20 16:56:30 +1100

    Initial project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app>
+        <app-navigation></app-navigation>
+        <v-content transition="slide-x-transition">
+            <router-view></router-view>
+            <hr>
+            <router-link to="/">Home</router-link>
+            |
+            <router-link to="/about">About</router-link>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+import AppNavigation from '@/components/AppNavigation';
+
+export default {
+    name: 'App',
+    components: {
+        AppNavigation
+    }
+};
+</script>
+
+<style>
+</style>
